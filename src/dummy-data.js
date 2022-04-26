08304a4768981d57e97c858ae3726b75eb1c94d0
amine aqebli <aqebli36@gmail.com>
2022-04-26 17:37:53 +0000

    Start Converting

---
+++ src/dummy-data.js
@@ -0,0 +1,39 @@
+export default [{
+        id: 'ahmed',
+        fullName: 'Ahmed Aqebli',
+        projects: [
+            { id: 'ahmed_p1', title: 'Record the Vue course' },
+            { id: 'ahmed_p2', title: 'Create more courses' },
+            { id: 'ahmed_p3', title: 'Keep content updated' }
+        ]
+    },
+    {
+        id: 'fatima',
+        fullName: 'Fatima Aqebli',
+        projects: [
+            { id: 'fatima_p1', title: 'Create more courses' },
+            { id: 'fatima_p2', title: 'Dive into data science topics' },
+            { id: 'fatima_p3', title: 'Various things' }
+        ]
+    },
+    {
+        id: 'kamal',
+        fullName: 'Kamal Aqebli',
+        projects: [
+            { id: 'kamal_p1', title: 'Create agenda for next months' },
+            { id: 'kamal_p2', title: 'Explore new topics' }
+        ]
+    },
+    {
+        id: 'abdo',
+        fullName: 'Abdo Aqebli',
+        projects: []
+    },
+    {
+        id: 'amine',
+        fullName: 'Amine Aqebli',
+        projects: [
+            { id: 'amine_p1', title: 'Create PFE' }
+        ]
+    }
+];
